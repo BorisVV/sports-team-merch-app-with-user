dadd0c2a806bf8b45f28aa2b0623afa6921b6f23
BorisVV <bcarlof@gmail.com>
2018-07-16 00:19:21 -0500

    Impor Issue 1.0 requirements upgraded, wtforms-2.2.1, flask-wtf-0.14.2, changed forms.py Forms deprecated in flask-wtf.

--- app_sport_team/forms.py
+++ app_sport_team/forms.py
@@ -1,14 +1,14 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
 
-class SignupForm(Form):
+class SignupForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired("Please enter a name.")])
     email = StringField('Email', validators=[DataRequired("Please enter a valid email.")])
     password = PasswordField('Password', validators=[DataRequired("Please enter a password.")])
     submit = SubmitField('Submit')
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired("Please enter your email address"), Email("Please enter a valid email address")])
     password = PasswordField('Password', validators=[DataRequired("Please enter a password")])
     submit = SubmitField("Sign in")
